6c1986fc574d2c6e53409cc4912790a362d7a30a
Zweihänder <zweidev@zweihander.me>
2021-10-17 01:23:39 -0400

    refactor(olc): major simplification
    We only need :face from org-link-set-parameters to achieve color.
    That simplifies the entire codebase and removes agenda related perf
    issues.

--- org-link-colorize.el
+++ org-link-colorize.el
@@ -73,116 +73,26 @@ CAR and the foreground color as the CDR. Foreground color can be obtained using
                        (string :tag "File type") (string :tag "Foreground color")))
   :group 'org-link-colorize)
 
-(defun org-link-colorize--get-element (position)
-  "Return the org element of link at the `POSITION'."
-  (save-excursion
-    (goto-char position)
-    ;; don't beautify in those elements
-    (unless (or (org-at-property-p)
-                (org-at-clock-log-p)
-                (org-at-encrypted-entry-p))
-      (org-element-context))))
-
-(defun org-link-colorize--add-overlay-marker (start end)
-  "Add 'org-link-colorize on link text-property between START and END."
-  (put-text-property start end 'type 'org-link-colorize))
-
-(defun org-link-colorize--add-color (start end description color)
-  "Change to COLOR between START and END with DESCRIPTION."
-  (put-text-property
-   start end
-   'display
-   (propertize
-    (propertize description 'face `(:foreground ,color)))))
-
-(defun org-link-colorize--display-not-exist (start end description)
-  "Display strike-through on START and END with DESCRIPTION."
-  (put-text-property
-   start end
-   'display
-   (propertize
-    (propertize description 'face '(:strike-through t)))))
-
-(defun org-link-colorize-display (start end path bracket-p)
-  "Display icon for the link type based on PATH from START to END."
-  (when (eq (car (org-link-colorize--get-element start)) 'link)
-    (save-match-data
-      (let* ((link-element (org-link-colorize--get-element start))
-             (raw-link (org-element-property :raw-link link-element))
-             (type (org-element-property :type link-element))
-             (desired-color (cdr (seq-find (lambda (k)
-                                             (equal (car k) type))
-                                           org-link-colorize-link-colors)))
-             (description (or (and (org-element-property :contents-begin link-element) ; in raw link case, it's nil
-                                   (buffer-substring-no-properties
-                                    (org-element-property :contents-begin link-element)
-                                    (org-element-property :contents-end link-element)))
-                              ;; when description not exist, use raw link for raw link case.
-                              raw-link)))
-        (when bracket-p (ignore))
-        (when desired-color
-          (org-link-colorize--add-color start end description desired-color))
-        (when (and (equal type "file") (not (file-exists-p path)))
-          (org-link-colorize--display-not-exist start end description))
-        (org-link-colorize--add-overlay-marker start end)))))
-
-;;; hook on headline expand
-(defun org-link-colorize-headline-cycle (&optional state)
-  "Function to be executed on `org-cycle-hook' STATE."
-  (pcase state
-    ('subtree (ignore))
-    ('children (ignore))
-    ('folded
-     (org-link-colorize-clear state))
-    (_ (ignore)))
-  ;; PERFORMANCE: benchmark this.
-  (org-restart-font-lock))
-
-;;; toggle org-link-colorize text-properties
-(defun org-link-colorize--clear-text-properties (&optional begin end)
-  "Clear all org-link-colorize text-properties between BEGIN and END."
-  (let ((point (or begin (point-min)))
-        (bmp (buffer-modified-p)))
-    (while (setq point (next-single-property-change point 'display))
-      (when (and (< point (or end (point-max)))
-                 (get-text-property point 'display)
-                 (eq (get-text-property point 'type) 'org-link-colorize))
-        (remove-text-properties
-	 point (setq point (next-single-property-change point 'display))
-	 '(display t))))
-    (set-buffer-modified-p bmp)))
-
-(defun org-link-colorize-clear (&optional state)
-  "Clear the text-properties of `org-link-colorize' globally.
-Or clear org-link-colorize if headline STATE is folded."
-  (if (eq state 'folded)
-      ;; clear in current folded headline
-      (save-excursion
-        (save-restriction
-          (org-narrow-to-subtree)
-          (let* ((begin (point-min))
-                 (end (save-excursion (org-next-visible-heading 1) (point))))
-            (org-link-colorize--clear-text-properties begin end))))
-    ;; clear in whole buffer
-    (org-link-colorize--clear-text-properties))
-  (org-restart-font-lock))
-
 ;;;###autoload
 (defun org-link-colorize-enable ()
   "Enable `org-link-colorize'."
   (when (display-graphic-p)
     (dolist (link-type (mapcar #'car org-link-parameters))
-      (org-link-set-parameters link-type :activate-func #'org-link-colorize-display))
-    (add-hook 'org-cycle-hook #'org-link-colorize-headline-cycle)
+      (let ((desired-color (cdr (seq-find (lambda (k)
+                                            (string= (car k) link-type))
+                                          org-link-colorize-link-colors))))
+        (when desired-color
+          (org-link-set-parameters link-type
+                                   :face `(:inherit org-link
+                                           :foreground ,desired-color)))))
     (org-restart-font-lock)))
 
 ;;;###autoload
 (defun org-link-colorize-disable ()
   "Disable `org-link-colorize'."
   (dolist (link-type (mapcar #'car org-link-parameters))
-    (org-link-set-parameters link-type :activate-func t))
-  (remove-hook 'org-cycle-hook #'org-link-colorize-headline-cycle)
-  (org-link-colorize-clear))
+    (org-link-set-parameters link-type :face t))
+  (org-restart-font-lock))
 
 ;;;###autoload
 (define-minor-mode org-link-colorize-mode
